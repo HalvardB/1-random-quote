0e005a41361f6769433bf9ec8717d598d62e550c
Halvard Bastiansen <halvard.bastiansen@gmail.com>
2018-05-04 01:45:42 +0200

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-/* Documentation
+/* Summary
 - See file named quotes.js for the array of quotes.
-- When you first open the html, you will see a random quote. Not the static HTML quote.
+- When you first run the script, you will see a random quote. Not the static HTML quote.
 - If you click on the button you can see a new random quote.
 - If you wait longer than 5 seconds, the page will automatically change the quote for you.
+- You can press the pause button, to stop the quote changes. The button will also dissapear if you click it.
 - The background color changes to a random color every time the quote changes.
 */
 
@@ -11,7 +12,9 @@ const quoteOutput = document.getElementsByClassName("quote")[0];
 const sourceOutput = document.getElementsByClassName("source")[0];
 const categoryOutput = document.getElementsByClassName("category")[0];
 const button = document.getElementById('loadQuote');
-let newQuoteCountdown = window.setInterval(loading, 5000); //
+const pauseButton = document.getElementById('pauseButton');
+let newQuoteCountdown = window.setInterval(printQuote, 5000); // Changing the quote automatically after 5 seconds.
+const quoteIdArray = [];
 
 // Random quote generator
 function getRandomQuote(){
@@ -21,53 +24,59 @@ function getRandomQuote(){
 }
 
 // Print function
-function printQuote(randomQuote){
+function printQuote(){
+
+  // Calling the getRandomQuote function and storing the value in a variable.
+  getRandomQuote();
+  const randomQuote = getRandomQuote();
+
+  // Changing the background color
+  changeBackgound();
 
   // Printing the Quote and Category
   quoteOutput.innerHTML = randomQuote.quote;
   categoryOutput.innerHTML = randomQuote.category;
 
-  // I added on purpuse only quotes that had both citiation and year, or none of them.
-  if(randomQuote.citation !== undefined && randomQuote.year !== undefined){
-    sourceOutput.innerHTML = randomQuote.source + ", <i>" + randomQuote.citation + "</i>, " + randomQuote.year;
-  } else {
-    sourceOutput.innerHTML = randomQuote.source;
+  // Printing Source, Citation and Year
+  let text = "";
+
+  if(randomQuote.source !== undefined){
+    text += randomQuote.source;
   }
+  if(randomQuote.citation !== undefined){
+    text += ", <i>" + randomQuote.citation + "</i>";
+  }
+  if(randomQuote.year !== undefined){
+    text += ", " + randomQuote.year;
+  }
+  sourceOutput.innerHTML = text;
 }
 
-// Button click actions
+// New quote button click
 button.addEventListener("click", () =>{
-  const randomQuote = getRandomQuote()
-  printQuote(randomQuote);
-  changeBackgound();
+  printQuote();
+  pauseButton.style.display = "block";
 
   // Resets the countdown timer.
   clearCountdown();
-  newQuoteCountdown = window.setInterval(loading, 5000);
+  newQuoteCountdown = window.setInterval(printQuote, 5000);
 });
 
-// Change background color. Function is called on button click and in the loading function.
+// Pause button - stopping the countdown.
+pauseButton.addEventListener("click", () =>{
+  clearCountdown()
+  pauseButton.style.display = "none";
+});
+
+// Change background color.
 function changeBackgound(){
-  document.body.style.backgroundColor = randomColor();
+  document.body.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
 }
 
-// Create a random color code
-function randomColor() {
-  const color = "#"+((1<<24)*Math.random()|0).toString(16); // Thank you Stackoverflow!
-  return color;
-}
-
-// To clear the countdown. Only called when the button is clicked.
+// To clear the countdown.
 function clearCountdown() {
   clearInterval(newQuoteCountdown);
 }
 
-// To make sure the visitor see a random quote when first loading the page.
-function loading(){
-  const randomQuote = getRandomQuote()
-  printQuote(randomQuote);
-  changeBackgound();
-}
-
-// Calling the loading() function.
-loading();
+// To make sure the visitor see a random quote when first opening the page.
+printQuote();
