7cc85bad901bfd907eef9d418f5ba904718ebaa7
Halvard Bastiansen <halvard.bastiansen@gmail.com>
2018-05-03 19:26:10 +0200

    Initial commit

---
+++ js/quotes.js
@@ -0,0 +1,63 @@
+// Quote directory
+const quotes = [
+  {
+    quote: "Screw it, let's do it!",
+    source: "Richard Brandson",
+    citation: "Like a Virgin",
+    year: "2012",
+    category: "Business advice"
+  },
+  {
+    quote: "Don't wish it was easier, wish you where better.",
+    source: "Jim Rohn",
+    category: "Motivation"
+  },
+  {
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    source: "Napoleon Hill",
+    citation: "Think and grow rich",
+    year: "1937",
+    category: "Motivation"
+  },
+  {
+    quote: "Don't cry because it's over, smile because it happened.",
+    source: "Dr Seuss",
+    category: "Happiness"
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    source: "Oscar Wilde",
+    category: "Honesty"
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    source: "Mae West",
+    category: "Humor"
+  },
+  {
+    quote: "I have a dream!",
+    source: "Martin Luther King jr.",
+    citation: "Speech",
+    year: "1963",
+    category: "Motivation"
+  },
+  {
+    quote: "True love is rare, and it's the only thing that gives life real meaning.",
+    source: "Nicolas Sparks",
+    citation: "Message in a Bottle",
+    year: "1998",
+    category: "Romance"
+  },
+  {
+    quote: "We are all in the gutter, but some of us are looking at the stars",
+    source: "Oscar Wilde",
+    citation: "Lady Windermere's Fan",
+    year: "1893",
+    category: "Optimism"
+  },
+  {
+    quote: "I have not failed. I've just found 10.000 ways that won't work.",
+    source: "Thomas E. Edison",
+    category: "Inspirational"
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,75 @@
+/* Documentation
+- See file named quotes.js for the array of quotes.
+- When you first open the html, you will see a random quote. Not the static HTML quote.
+- If you click on the button you can see a new random quote.
+- If you wait longer than 5 seconds, the page will automatically change the quote for you.
+- The background color changes to a random color every time the quote changes.
+
+- I tried to add a feature where you
+*/
+
+let quoteId;
+const quoteOutput = document.getElementsByClassName("quote")[0];
+const sourceOutput = document.getElementsByClassName("source")[0];
+const categoryOutput = document.getElementsByClassName("category")[0];
+const button = document.getElementById('loadQuote');
+let newQuoteCountdown = window.setInterval(loading, 5000); //
+
+// Random quote generator
+function getRandomQuote(){
+  quoteId = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[quoteId];
+  return quote;
+}
+
+// Print function
+function printQuote(randomQuote){
+
+  // Printing the Quote and Category/Title
+  quoteOutput.innerHTML = randomQuote.quote;
+  categoryOutput.innerHTML = randomQuote.category;
+
+  // I added on purpuse only quotes that had both citiation and year, or none of them.
+  if(randomQuote.citation !== undefined && randomQuote.year !== undefined){
+    sourceOutput.innerHTML = randomQuote.source + ", <i>" + randomQuote.citation + "</i>, " + randomQuote.year;
+  } else {
+    sourceOutput.innerHTML = randomQuote.source;
+  }
+}
+
+// Button click actions
+button.addEventListener("click", () =>{
+  const randomQuote = getRandomQuote()
+  printQuote(randomQuote);
+  changeBackgound();
+
+  // Resets the countdown.
+  clearCountdown();
+  newQuoteCountdown = window.setInterval(loading, 5000);
+});
+
+// Change background color. Function is called on button click and in the loading function.
+function changeBackgound(){
+  document.body.style.backgroundColor = randomColor();
+}
+
+// Create a random color
+function randomColor() {
+  const color = "#"+((1<<24)*Math.random()|0).toString(16); // Thank you Stackoverflow!
+  return color;
+}
+
+// To clear the countdown. Only called when the button is clicked.
+function clearCountdown() {
+  clearInterval(newQuoteCountdown);
+}
+
+// To make sure the visitor see a random quote when first loading the page.
+function loading(){
+  const randomQuote = getRandomQuote()
+  printQuote(randomQuote);
+  changeBackgound();
+}
+
+// Calling the loading() function.
+loading();
